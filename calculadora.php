ccff3789dfa3a5141a5eed64fc6fb3fd08f1f4fb
Gui <137961728+Guisag777@users.noreply.github.com>
2023-07-11 21:56:50 -0300

    Create calculadora.php

---
+++ calculadora.php
@@ -0,0 +1,23 @@
+<?php
+
+$Val1 = 6;
+$Val2 = 2;
+$Opr = "/";
+$Result = 0;
+
+if ($Opr=="+") {
+    $Result=$Val1+$Val2;
+}// O valor é 8
+else if ($Opr=="-") {
+    $Result=$Val1-$Val2;
+}// O valor é 4
+else if ($Opr=="/") {
+    $Result=$Val1/$Val2;
+}// O valor é 3
+else {
+    $Result=$Val1*$Val2;
+}
+
+// O valor é 12
+
+echo "O resultado é ". $Result;
