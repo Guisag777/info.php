e29941bb3c4f1344c98d3edccabfba91167446f4
Gui <137961728+Guisag777@users.noreply.github.com>
2023-08-01 21:55:24 -0300

    Novas funcionalidades

--- calculadora.php
+++ calculadora.php
@@ -1,11 +1,11 @@
 <?php
 
-$Val1 = 6;
-$Val2 = 2;
-$Opr = "^";
+$Val1 = 100;
+$Val2 = 10;
+$Opr = "ma";
 $Result = 0;
 
-
+//Operadores básicos
 if ($Opr=="+") {
     $Result=$Val1+$Val2;
 }
@@ -22,12 +22,37 @@ else if ($Opr=="^") {
     $Result=pow($Val1, $Val2);
 }
 
-echo "O resultado é ". $Result;
+//Percentual
+//Desconto=percentual de desconto
+//Result=valor a ser pago
 
+if ($Opr == "perc") {
+    $Desconto = ($Val1 * $Val2) / 100;
+    $Result= $Val1 - $Desconto;
+}
+
+
+//Acrescimo=percentual de valor acrescentado 
+//Result=Valor a ser pago
+if ($Opr == "perc+") {
+    $Acrescimo = ($Val1 * $Val2) / 100;
+    $Result= $Val1 + $Acrescimo;
+}
+
+//Média aritimética
+if ($Opr == "ma") {
+    $Result=($Val1+$Val2)/2; 
+}
+
+echo " O resultado é ". $Result;
+
+//Par ou ímpar
 if ($Result%2==0) {
     echo " (Número par)";
 }
-else {
+  else {
     echo " (Número ímpar)";
 }
+
+
  
