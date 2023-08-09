44925e66c3d82709716964c2172d2c014da98fb0
Guilherme Secco de Aguiar <137961728+Guisag777@users.noreply.github.com>
2023-08-08 21:31:04 -0300

    Laços de repetição e novas funcionalidades

--- calculadora.php
+++ calculadora.php
@@ -1,34 +1,39 @@
 <?php
 
-$Val1 = 100;
+$Val1 = 2;
 $Val2 = 10;
-$Opr = "ma";
+$Opr = "Tab";
 $Result = 0;
 
 //Operadores básicos
 if ($Opr=="+") {
     $Result=$Val1+$Val2;
+    echo " O resultado é ". $Result;
 }
 else if ($Opr=="-") {
     $Result=$Val1-$Val2;
+    echo " O resultado é ". $Result;
 }
 else if ($Opr=="/") {
     $Result=$Val1/$Val2;
+    echo " O resultado é ". $Result;
 }
 else if ($Opr=="*") {
     $Result=$Val1*$Val2;
+    echo " O resultado é ". $Result;
 }
 else if ($Opr=="^") {
     $Result=pow($Val1, $Val2);
+    echo " O resultado é ". $Result;
 }
 
 //Percentual
 //Desconto=percentual de desconto
 //Result=valor a ser pago
-
 if ($Opr == "perc") {
     $Desconto = ($Val1 * $Val2) / 100;
     $Result= $Val1 - $Desconto;
+    echo " O resultado é ". $Result;
 }
 
 
@@ -37,22 +42,38 @@ if ($Opr == "perc") {
 if ($Opr == "perc+") {
     $Acrescimo = ($Val1 * $Val2) / 100;
     $Result= $Val1 + $Acrescimo;
+    echo " O resultado é ". $Result;
 }
 
 //Média aritimética
 if ($Opr == "ma") {
     $Result=($Val1+$Val2)/2; 
+    echo " A média aritimética é: ". $Result;
 }
 
-echo " O resultado é ". $Result;
-
-//Par ou ímpar
-if ($Result%2==0) {
-    echo " (Número par)";
+//Contador com laços de repetição
+//$Val1 é o número até qual você deseja contar
+//$c é sempre igual a 0
+if ($Opr == "cont") {
+    echo "Contando...\n";
+    for ($c=0; $c<=$Val1; $c++)
+    {
+        echo $c."<br>";
+    }
 }
-  else {
-    echo " (Número ímpar)";
+
+//Tabuada com laços de repetição
+//$Val1 é o número da tabuada
+//$c é sempre igual a 0, e será contado até 10 sendo multiplicado pelo $Val1
+if ($Opr == "Tab") {
+    
+    echo "Tabuada do ". $Val1."<br>";
+
+for($c=0; $c <= 10; $c++)
+{
+    echo $c."X".$Val1."=";
+    echo $c * $Val1."<br>";
 }
-
-
+}
  
+
